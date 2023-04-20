09bc66aea9f76e389e04bde38869cde9e0ef5616
vin <didekqls93@gmail.com>
2023-04-20 13:11:49 +0900

    how to do style

--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
 export default function Home() {
+  let name = "park";
+  let link = "http://google.com";
   return (
     <div>
-      <h4 className="red">Hi!</h4>
+      <h4 className="title">Apple Fresh</h4>
+      <p className="title-sub">by {name}</p>
+      <a href={link}>go to google</a>
     </div>
   );
 }
+
+// style 적용하는 방법
