c0d5664fc2b8783376b0c317431acbf27819f26b
vin <didekqls93@gmail.com>
2023-04-21 15:24:14 +0900

    feat : create list (hard coding)

--- app/list/page.js
+++ app/list/page.js
@@ -1,14 +1,19 @@
 //next.js에서는 폴더를 생성하면 자동적으로 '폴더 명' = 'url 주소'가 된다
 
 export default function List() {
+  let products = ["Tomatoes", "Pasta", "Coconet"];
+
   return (
     <div>
       <h4 className="title">List</h4>
       <div className="food">
-        <h4>Product 1 $40</h4>
+        <h4>{products[0]} $40</h4>
       </div>
       <div className="food">
-        <h4>Product 2 $40</h4>
+        <h4>{products[1]} $40</h4>
+      </div>
+      <div className="food">
+        <h4>{products[2]} $40</h4>
       </div>
     </div>
   );
